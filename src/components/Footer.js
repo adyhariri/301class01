8d1e883fcf65242f710f12693c609fd40add18ce
adyhariri <mahmoudalhariri1994@gmail.com>
2021-06-07 19:28:28 +0300

    fixing lab01

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+class Footer extends React.Component {
+  render(){
+    return (
+        
+      <footer> 
+        authorized by mahmoud alhariri
+      </footer>
+           
+    );
+  }
+}
+export default Footer;
