ccd2fb9cc293cf716ce69050a0df6cf04e883d3c
adyhariri <mahmoudalhariri1994@gmail.com>
2021-06-09 15:13:27 +0300

    all done on lab02

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,25 +3,23 @@ import HornedBeasts from './HornedBeasts';
 import 'bootstrap/dist/css/bootstrap.min.css';
 // import CardColumns from 'react-bootstrap/CardColumns';
 import CardGroup from 'react-bootstrap/CardGroup';
-import Cards from './Cards.json';
-
+import cards from '../data/cards.json';
 class Main extends React.Component {
-  render (){
+  render() {
     return (
-      <main>
-        <section>
-          <CardGroup>
-            {Cards.map(item=>(
-              <HornedBeasts
-                imageUrl={item.image_url}
-                title={item.title}
-                descrption={item.description}
-              />
-            ))}
-          </CardGroup>
-        </section>
-      </main>
+      <section>
+        <CardGroup>
+          {cards.map(item => (
+            <HornedBeasts
+              imageUrl={item.image_url}
+              title={item.title}
+              descrption={item.description}
+            />
+          ))}
+        </CardGroup>
+      </section>
     );
   }
 }
 export default Main;
+
